d736cef79a78ea7f01b753d6846de1b99b6976f2
aquib98 <aquib.syed1997@gmail.com>
2023-04-12 20:13:00 -0700

    Added Player At Zero Position File

---
+++ PlayerStartsAtZero.java
@@ -0,0 +1,16 @@
+package CalculatorProgram;
+
+public class PlayerStartsAtZero {
+	/*
+	 * player starts the game from zero
+	 */
+	public static void main(String[] args) {
+
+		Player p1 = new Player();
+		int position = 0;
+
+		System.out.println("Game start ");
+                
+        System.out.println("Position of player "+p1+" is"+position);
+        }
+}
